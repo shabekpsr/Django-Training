2ce09ca4d48183e8b2ef451b698605734974a8c1
Shabekpsr <shabekpsr@gmail.com>
2022-11-15 12:31:31 +0530

    SignInFormAdded

--- authcrud_prj/authcrud_app/views.py
+++ authcrud_prj/authcrud_app/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    #return render(request,'index.html')
-    return HttpResponse("Hello World")
+    return render(request,'index.html')
+    #return HttpResponse("Hello World")
