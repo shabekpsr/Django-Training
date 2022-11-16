c39b3e64c58d5ed1db5014ac80213f276c100291
Shabekpsr <shabekpsr@gmail.com>
2022-11-16 15:28:33 +0530

    SignInAndSignUpDesignandRedirectFinishedSuccessfully

--- authcrud_prj/authcrud_app/urls.py
+++ authcrud_prj/authcrud_app/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('',views.index,name='index'),
+    path('signup/',views.signup,name='signup'),
+    path('signup/login',views.login,name='login'),
 ]
--- authcrud_prj/authcrud_app/views.py
+++ authcrud_prj/authcrud_app/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import HttpResponseRedirect, redirect, render
+from django.template import loader
+from django.urls import reverse
+from django.shortcuts import render
+
 
 # Create your views here.
 def index(request):
     return render(request,'index.html')
     #return HttpResponse("Hello World")
+def login(request):
+    context={}
+    #return render(request,'index.html',context)
+    #return HttpResponse('index.html')
+    #return HttpResponseRedirect(reverse('index.html'))
+    #return redirect('index')
+    return HttpResponseRedirect(reverse('index'))
+def signup(request):
+    return render(request,'signup.html')
+    
+    
